6cc94a9a2a0779dc62897baf61b405b1d94e70dd
lucadonnoh <donnoh99@gmail.com>
2022-06-21 22:56:54 +0200

    redesign

--- components/NftCard.js
+++ components/NftCard.js
@@ -1,26 +1,42 @@
 import Link from "next/link";
 import React from "react";
+import { Box, Image, Badge, Text } from "@chakra-ui/react"
 
 export default function NftCard({name, description, thumbnail, collectionAddress, nftId}) {
     return (
-        <div style={{
-            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-            transition: "0.3s",
-            width: "auto"
-        }}>
-            <img src= {thumbnail} alt="Nft"></img>
-            <div style={{
-                padding: "2px 16px"
-            }}>
-                <h4><b>{name}</b></h4>
-                <p>Description: {description}</p>
+        <Box w="xs" borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2em">
+            <Image src={thumbnail} fallbackSrc="https://via.placeholder.com/500" alt={"Collection's Cover image"} w="100%" objectFit="cover" />
+            <Box p="6">
+                <Box
+                    mt='1'
+                    fontWeight='semibold'
+                    as='h3'
+                    fontSize="xl"
+                    lineHeight='tight'
+                    noOfLines={1}
+                    >
+                    {name}
+                </Box>
+                <Box
+                    mt='1'
+                    fontWeight='regular'
+                    as='h4'
+                    lineHeight='tight'
+                    noOfLines={2}
+
+                    >
+                    <Text as="i">{description}</Text>
+                </Box>
+                <Box fontWeight="bold">
+                    PRICE: 11 ETH
+                </Box>
                 <Link href={`/nft/${collectionAddress}/${nftId}`}>
                     <a style={{
                         color: "red",
                         textDecoration: "underline"
                     }}>See more</a>
                 </Link>
-            </div>
-        </div>
+            </Box>
+        </Box>
     )
 }
