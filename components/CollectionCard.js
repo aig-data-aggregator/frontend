4c09a45d2ce65316627b6c17782ea2ea7768df33
lucadonnoh <donnoh99@gmail.com>
2022-06-21 22:56:48 +0200

    redesign

--- components/CollectionCard.js
+++ components/CollectionCard.js
@@ -1,27 +1,56 @@
 import React from "react";
 import Link from "next/link"
 
+import { Box, Image, Badge, Text } from "@chakra-ui/react"
+
 export default function CollectionCard({name, coverUrl, description, address}) {
     return (
-        <div style={{
-            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-            transition: "0.3s",
-            width: "auto",
-            padding: "1em"
-        }}>
-            <img src={coverUrl} alt="Collection's Cover image"></img>
-            <div style={{
-                padding: "2px 16px"
-            }}>
-                <h4><b>{name}</b></h4>
-                <p>Description: {description}</p>
+        <Box w="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" h="md">
+            <Image src={coverUrl} fallbackSrc="https://via.placeholder.com/500" alt={"Collection's Cover image"} w="100%" h="15em" overflow="hidden" objectFit="cover" />
+            <Box p="6">
+                <Box display="flex" alignItems="baseline">
+                    <Badge borderRadius="full" px="2" colorScheme="teal">NFT</Badge>
+                    <Box
+                        color='gray.500'
+                        fontWeight='semibold'
+                        letterSpacing='wide'
+                        fontSize='xs'
+                        textTransform='uppercase'
+                        ml='2'
+                    >
+                        10000 items
+                    </Box>
+                </Box>
+                <Box
+                    mt='1'
+                    fontWeight='semibold'
+                    as='h3'
+                    fontSize="xl"
+                    lineHeight='tight'
+                    noOfLines={1}
+                    >
+                    {name}
+                </Box>
+                <Box
+                    mt='1'
+                    fontWeight='regular'
+                    as='h4'
+                    lineHeight='tight'
+                    noOfLines={2}
+
+                    >
+                    <Text as="i">{description}</Text>
+                </Box>
+                <Box fontWeight="bold">
+                    FLOOR: 7 ETH
+                </Box>
                 <Link href={`/collection/${address}`}>
                     <a style={{
                         color: "red",
                         textDecoration: "underline"
                     }}>See more</a>
                 </Link>
-            </div>
-        </div>
+            </Box>
+        </Box>
     )
 }
